bb3722f3426b7b592bb4b3edc57d38de76082ea0
sdj3261 <63071967+sdj3261@users.noreply.github.com>
2022-06-18 09:07:20 +0900

    COOKIE , LOADCONFIG 실습

--- src/main/java/ex04/SetCookieValue.java
+++ src/main/java/ex04/SetCookieValue.java
@@ -26,7 +26,7 @@ public class SetCookieValue extends HttpServlet {
         Date d = new Date();
         Cookie c = new Cookie("cookieTest", URLEncoder.encode("JSP프로그래밍입니다.", "utf-8"));
         c.setMaxAge(24 * 60 * 60);
-        //c.setMaxAge(-1);  //세션 쿠키를 생성합니다.
+        c.setMaxAge(-1);  //세션 쿠키를 생성합니다.
         response.addCookie(c);
         out.println("현재시간 : " + d);
         out.println("<br> 문자열을 Cookie에 저장합니다.");

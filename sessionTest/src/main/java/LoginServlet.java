e79728047e61a27e58889c660604a89de5532f37
sdj3261 <63071967+sdj3261@users.noreply.github.com>
2022-06-24 09:36:05 +0900

    필터와 리스너 관련 커밋

--- sessionTest/src/main/java/LoginServlet.java
+++ sessionTest/src/main/java/LoginServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpSession;
  * Servlet implementation class LoginServlet
  */
 
-@WebServlet("/login")
+//@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
